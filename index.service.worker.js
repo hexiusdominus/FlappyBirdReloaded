8210f7a878efa9dd80efa433d91a1e3b08651d83
xxx <hexury89@gmail.com>
2025-07-18 19:02:24 +0700

    share twitter

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1752737331|5767604513';
+const CACHE_VERSION = '1752835659|11615171637';
 /** @type {string} */
 const CACHE_PREFIX = 'Flappy Bird-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
